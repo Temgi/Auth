bbe9d5dc189e92ca0cbb6a8213786e0ba067287f
Rui Temgi <ruisaraivasp@gmail.com>
2023-08-08 20:45:22 -0300

    Connecting Applications to PostgreSQL

---
+++ Statefull/statefull-auth-api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO public.user (id, username, password) VALUES (1, 'user.test', '$2a$10$E0w31hw/iWRU32MsG0D9XuNJnQC4zA0wLMLfn.lNv2DEyr/dzea2K');
+
+INSERT INTO public.user (id, username, password) VALUES (2, 'user.new.test', '$2a$10$E0w31hw/iWRU32MsG0D9XuNJnQC4zA0wLMLfn.lNv2DEyr/dzea2K')
